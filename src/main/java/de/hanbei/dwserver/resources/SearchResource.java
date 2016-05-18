3b275f67da288dc43d15ea76424bd15bf61b0217
Florian Schulz <hanbei@hanbei.de>
2016-05-18 21:54:50 +0200

    First searcher fred_de

--- src/main/java/de/hanbei/dwserver/resources/SearchResource.java
+++ src/main/java/de/hanbei/dwserver/resources/SearchResource.java
@@ -2,12 +2,15 @@ package de.hanbei.dwserver.resources;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import com.google.common.io.Resources;
 import com.google.common.util.concurrent.Uninterruptibles;
 import de.hanbei.dwserver.model.Offer;
 import de.hanbei.dwserver.model.StaticResponse;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -16,6 +19,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -23,6 +27,7 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.io.Resources.getResource;
 
 @Path("/search")
 public class SearchResource {
@@ -41,9 +46,21 @@ public class SearchResource {
 
     @Path("/{searcher}")
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response respondAsSearcher(@PathParam("searcher") String searcher, @QueryParam("q") String query, @QueryParam("size") int size) {
-        return getResponse(size);
+    public Response respondAsSearcher(@PathParam("searcher") String searcher,
+                                      @QueryParam("q") String query,
+                                      @QueryParam("size") int size,
+                                      @QueryParam("country") @DefaultValue("de") String country) {
+        try {
+            String format = Resources.toString(getResource(searcher + "/format"), Charsets.UTF_8);
+
+            URL resource = getResource(searcher + "/response_" + country);
+            String s = Resources.toString(resource, Charsets.UTF_8);
+            Uninterruptibles.sleepUninterruptibly(random.nextInt(2000), TimeUnit.MILLISECONDS);
+            return Response.ok(s).type(format).build();
+        } catch (IOException | IllegalArgumentException e) {
+            return Response.status(Response.Status.NO_CONTENT).build();
+        }
+
     }
 
     private Response getResponse(@QueryParam("size") int size) {
